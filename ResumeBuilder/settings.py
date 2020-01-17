a5ab1c7f0adcbea74e49912d194ab756290c81e4
Udit <500062348@stu.upes.ac.in>
2020-01-17 10:37:00 +0530

    bug fix

--- ResumeBuilder/settings.py
+++ ResumeBuilder/settings.py
@@ -24,9 +24,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://udit107710.github.io", "https://resume-builder-v1.herokuapp.com"]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+       'http://localhost:80',
+    'https://udit107710.github.io'
+)
 
 # Application definition
 
