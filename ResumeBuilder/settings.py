331f204938d3922284ff9d3cb7618ac80881c4ee
Udit <500062348@stu.upes.ac.in>
2020-01-17 09:55:16 +0530

    CORS condfigured

--- ResumeBuilder/settings.py
+++ ResumeBuilder/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     # For TokenAuth
     'rest_framework',
     'rest_framework.authtoken',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
